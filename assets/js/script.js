d1c0a85a63779985b1aa099cbb46f044683c7a57
mcarena <mcarena.quijada@outlook.com>
2024-03-04 21:59:28 -0300

    Se incorporan cambios importantes en el codigo como el ordenamiento y documentacion del script

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,83 +2,64 @@
 let iife = (function() {
     // Función privada para insertar el video en el documento HTML
     // CReo 2 Funciones, publico y otra privada (sumanumero, es un Obj.)
-    function insertarElemento(url, id) {
-        const iframe = document.createElement("iframe");
-        iframe.setAttribute("src", url);
-        document.getElementById(id).appendChild(iframe);
+    function privada(url, id) {
+        id.setAttribute("src", url);
+      
     }
     // Función pública para insertar el video con parámetros (url, id)
     // Retorno de las funciones públicas
     return {
-        insertarElemento: (url, id) => insertarElemento(url, id)
+        publica: (url, id) => privada(url, id)
           }
 }) ();
 //Llamado al método público de la función iife
 // iife retorna un Objeto
-console.log("Valor de iife: ", iife)
-iife.insertarElemento(4,2);
+console.log("Valor de iife: ", iife);
+//iife.insertarElemento(4,2);
 
-  
-    // Clase padre 
-    class Multimedia {
-      constructor(url) {
-        let _url = url;
-  
-        //  obtener el atributo 
-        this.getUrl = function () {
-          return _url;
-        };
-  
-        // cambio en la url del video
-        this.setInicio = function () {
-          return "cambio en la URL del video";
-        };
-      }
+
+
+
+
+// Clase padre 
+class Multimedia {
+    constructor(url) {
+      this.url = url;
+
     }
-  
-    // Clase hija 
-    class Reproductor extends Multimedia {
-      constructor(url, id) {
-        super(url);
-        this.id = id;
+
+      // cambio en la url del video
+      setInicio() {
+        return "cambio en la URL del video";
       }
-  
-      // reproducir multimedia
-      playMultimedia() {
-        const url = this.getUrl();
-        insertarElemento(url, this.id);
-      }
-  
-      // modificar el tiempo de inicio url
-      setInicio(tiempo) {
-        const url = `${this.getUrl()}?start=${tiempo}`;
-        insertarElemento(url, this.id);
-      }
+}
+  // Clase hija 
+  class Reproductor extends Multimedia {
+    constructor(url, id) {
+      super(url);
+      this.id = id;
     }
-  
-    // Devolver solo lo necesario (público)
-    return {
-      Reproductor: Reproductor,
-    };
- 
-  
-  // Instancia clase hija para música
-  const reproductorMusica = new Reproductor(
-    "",
-    "musica"
-  );
 
-  reproductorMusica.playMultimedia();
-  
-  // Instancia clase hija para película
-  const reproductorPelicula = new Reproductor("https://www.youtube.com/embed/5PSNL1qE6VY","peliculas");
-  reproductorPelicula.playMultimedia();
+    // reproducir multimedia
+    playMultimedia() {
+        iife.publica(this.url, this.id)
+    }
 
-  
-  
-  // Instancia clase hija para serie
-  const reproductorSerie = new Reproductor("","series");
-  reproductorSerie.playMultimedia();
-  
-  //  tiempo de inicio para instancia
-  reproductorMusica.setInicio(30);
+    // modificar el tiempo de inicio url
+    setInicio(tiempo) {
+      this.id.setAttribute("src", `${this.url}?start=${tiempo}`);
+    }
+  }
+
+const idMusica = document.getElementById("musica");
+let musica = new Reproductor("",idMusica);
+musica.playMultimedia();
+
+const idPelicula = document.getElementById("peliculas");
+let pelicula = new Reproductor("https://www.youtube.com/embed/5PSNL1qE6VY",idPelicula);
+pelicula.playMultimedia();
+pelicula.setInicio(20);
+
+const idSerie = document.getElementById("series");
+let serie = new Reproductor("",idSerie);
+serie.playMultimedia();
